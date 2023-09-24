6177985e777f7de1c144a75531863e8840b740d0
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2023-09-24 08:59:27 +0530

    Update urls.py

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -6,13 +6,12 @@ from . import views
 
 app_name = 'firstapp'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^savedata/$', views.crud.savedata, name='savedata'),
-    url(r'^updatedata/$', views.crud.updatedata, name='updatedata'),
-    url(r'^([0-9]+)/editdata/$', views.crud.editdata, name='editdata'),
-    url(r'^([0-9]+)/deletedata/$', views.crud.deletedata, name='deletedata'),
-    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-
+    path('', views.IndexView.as_view(), name='index'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('savedata/', views.crud.savedata, name='savedata'),
+    path('updatedata/', views.crud.updatedata, name='updatedata'),
+    re_path(r'^(\d+)/editdata/$', views.crud.editdata, name='editdata'),
+    re_path(r'^(\d+)/deletedata/$', views.crud.deletedata, name='deletedata'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
